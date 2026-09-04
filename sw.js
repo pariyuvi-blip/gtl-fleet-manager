23b2db1e01dfdea590173609e0d5d5e05f3e9160
pariyuvi-blip <pari.yuvi@gmail.com>
2026-09-04 17:37:03 +0530

    Bump GTL cache to v9

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='gtl-v8';
-const ASSETS=['./','./index.html','./styles.css','./app-v8-core.js','./app-v8-trips.js','./app-v8-extra.js','./manifest.webmanifest','./icon-192.png','./icon-512.png','./icon-180.png'];
+const CACHE='gtl-v9';
+const ASSETS=['./','./index.html','./styles.css','./app-v8-core.js','./app-v8-trips.js','./app-v8-extra.js','./app-v9-enhancements.js','./manifest.webmanifest','./icon-192.png','./icon-512.png','./icon-180.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()])));
 self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request))));
