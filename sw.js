55a8f47d00ec0171014945868fec7775bf2666f5
pariyuvi-blip <pari.yuvi@gmail.com>
2026-09-04 19:21:58 +0530

    Refresh PWA cache for dashboard fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='gtl-v13';
+const CACHE='gtl-v14';
 const ASSETS=['./','./index.html','./styles.css','./app-v8-core.js','./app-v8-trips.js','./app-v8-extra.js','./app-v9-enhancements.js','./app-v10-expenses.js','./app-v11-dashboard.js','./app-v12-finance.js','./manifest.webmanifest','./icon-192.png','./icon-512.png','./icon-180.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()])));
