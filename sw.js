df45eab479b554b2b33a7f87d18d09d1278ef8a6
pariyuvi-blip <pari.yuvi@gmail.com>
2026-09-04 19:27:16 +0530

    Bump cache for dashboard recovery build

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='gtl-v14';
-const ASSETS=['./','./index.html','./styles.css','./app-v8-core.js','./app-v8-trips.js','./app-v8-extra.js','./app-v9-enhancements.js','./app-v10-expenses.js','./app-v11-dashboard.js','./app-v12-finance.js','./manifest.webmanifest','./icon-192.png','./icon-512.png','./icon-180.png'];
+const CACHE='gtl-v15';
+const ASSETS=['./','./index.html','./styles.css','./app-v8-core.js','./app-v8-trips.js','./app-v8-extra.js','./app-v9-enhancements.js','./app-v10-expenses.js','./app-v11-dashboard.js','./app-v12-finance.js','./app-v15-dashboard-fix.js','./manifest.webmanifest','./icon-192.png','./icon-512.png','./icon-180.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()])));
 self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request))));
